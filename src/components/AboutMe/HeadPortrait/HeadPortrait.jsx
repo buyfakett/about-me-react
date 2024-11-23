19443baccc735ccc06b439bba754b5490f4942d2
buyfakett <buyfakett@vip.qq.com>
2024-11-23 14:46:18 +0800

    优化头像显示

--- src/components/AboutMe/HeadPortrait/HeadPortrait.jsx
+++ src/components/AboutMe/HeadPortrait/HeadPortrait.jsx
@@ -1,18 +1,23 @@
 import React from "react";
 import {HeadPortraitCircle} from "./HeadPortraitCircle";
+import {motion} from "framer-motion";
 
 
 const HeadPortrait = ({headPortrait}) => {
     return (
-        <div className="dark:bg-gray-900 dark:text-gray-300">
+        <motion.div
+            className="dark:bg-gray-900 dark:text-gray-300"
+            initial={{opacity: 0, scale: 0.9}}
+            animate={{opacity: 1, scale: 1}}
+            transition={{duration: 0.3, delay: 0.5}}
+        >
             <HeadPortraitCircle>
                 <img
                     className="w-32 h-32 rounded-full object-cover"
                     src={headPortrait}
                     alt=""/>
             </HeadPortraitCircle>
-
-        </div>
+        </motion.div>
 
     );
 };
