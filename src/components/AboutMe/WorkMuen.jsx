b370fdd6892139796fed634b25fb7944bf862fe3
buyfakett <buyfakett@vip.qq.com>
2025-01-18 22:21:05 +0800

    完善项目经验

--- src/components/AboutMe/WorkMuen.jsx
+++ src/components/AboutMe/WorkMuen.jsx
@@ -64,25 +64,21 @@ const WorkProjectMuen = () => {
 
     const projects = [
         {
-            id: 1,
             title: '日志系统重构',
             content: `项目思路：旧日志系统ELK占用资源多查询慢；修改为rsyslog、filebeat等收集日志到kafka，再由vector入clickhouse
 项目成果：几十TB级日志查询能在秒级查询成功，且占用系统资源相对低`,
         },
         {
-            id: 2,
             title: '数据库备份方案重构',
             content: `项目思路：旧备份方案备份从未检查/测试，不同备份分散；修改为不同环境整合
     项目成果：判断备份文件大小等算法自动生成报表和录入Prometheus系统，极大的降低人工巡检的成本`,
         },
         {
-            id: 3,
             title: '监控/报警系统重构',
             content: `项目思路：旧监控系统分散、不全，项目监控几乎没有；修改为zabbix+Prometheus+grafana整合监控系统
     项目成果：zabbix主要监控服务器宿主机、虚拟机性能，Prometheus监控各种项目返回时间/占用率等指标、各种云平台余额/套餐剩余等`,
         },
         {
-            id: 4,
             title: '运维系统编写',
             content: `项目思路：上百个服务器、服务、域名信息没地方登记，测试、运营等同事经常私聊问；服务上线时自动上报版本信息，新增服务器就登记
     项目成果：用fastapi+vue完成编写，全公司权限分离自行查询上千个服务器、数据库，降低运维重复的工作`,
@@ -112,7 +108,7 @@ const WorkProjectMuen = () => {
                             exit={{ opacity: 0 }}
                             transition={{ duration: 0.5 }}>
                             <button
-                                className="absolute top-4 left-4 text-sm"
+                                className="absolute top-5 left-10 text-sm"
                                 onClick={() => setSelectedProject(null)}>
                                 ← 返回项目列表
                             </button>
@@ -133,32 +129,11 @@ const WorkProjectMuen = () => {
                             animate={{ opacity: 1 }}
                             exit={{ opacity: 0 }}
                             transition={{ duration: 0.5 }}>
-                            {/* 第一行 */}
-                            <div className="flex gap-4 h-1/2">
-                                {projects.slice(0, 3).map((project) => (
+                            <div className="grid grid-cols-2 gap-4 h-full">
+                                {projects.map((project, index) => (
                                     <motion.div
-                                        key={project.id}
-                                        className="flex-1 p-4 bg-white/10 hover:bg-white/20 transition-colors cursor-pointer"
-                                        whileHover={{ scale: 1.05 }}
-                                        onClick={() =>
-                                            setSelectedProject(project)
-                                        }>
-                                        <div className="text-center">
-                                            {project.title}
-                                        </div>
-                                    </motion.div>
-                                ))}
-                            </div>
-
-                            {/* 分割线 */}
-                            <div className="border-b border-white my-2 w-full" />
-
-                            {/* 第二行 */}
-                            <div className="flex gap-4 h-1/2">
-                                {projects.slice(3).map((project) => (
-                                    <motion.div
-                                        key={project.id}
-                                        className="flex-1 p-4 bg-white/10 hover:bg-white/20 transition-colors cursor-pointer"
+                                        key={index}
+                                        className="p-4 bg-white/10 hover:bg-white/20 transition-colors cursor-pointer flex items-center justify-center"
                                         whileHover={{ scale: 1.05 }}
                                         onClick={() =>
                                             setSelectedProject(project)
