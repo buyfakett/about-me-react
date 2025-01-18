d407c8a97066988216e7a2a3c9a44b4a533c480b
buyfakett <buyfakett@vip.qq.com>
2025-01-18 23:02:26 +0800

    项目经验稍微适配手机端 数据移动到配置文件

--- src/components/AboutMe/WorkMuen.jsx
+++ src/components/AboutMe/WorkMuen.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { NeonGradientCard } from '../magicui/NeonGradientCard';
 import { Image } from '@douyinfe/semi-ui';
-import { imgUrl, skipUrl } from '../../config';
+import { imgUrl, muenProjects, skipUrl } from '../../config';
 import { Button } from '@douyinfe/semi-ui';
 import { motion, AnimatePresence } from 'framer-motion';
 
@@ -62,32 +62,9 @@ const WorkMuen = () => {
 const WorkProjectMuen = () => {
     const [selectedProject, setSelectedProject] = useState(null);
 
-    const projects = [
-        {
-            title: '日志系统重构',
-            content: `项目思路：旧日志系统ELK占用资源多查询慢；修改为rsyslog、filebeat等收集日志到kafka，再由vector入clickhouse
-项目成果：几十TB级日志查询能在秒级查询成功，且占用系统资源相对低`,
-        },
-        {
-            title: '数据库备份方案重构',
-            content: `项目思路：旧备份方案备份从未检查/测试，不同备份分散；修改为不同环境整合
-    项目成果：判断备份文件大小等算法自动生成报表和录入Prometheus系统，极大的降低人工巡检的成本`,
-        },
-        {
-            title: '监控/报警系统重构',
-            content: `项目思路：旧监控系统分散、不全，项目监控几乎没有；修改为zabbix+Prometheus+grafana整合监控系统
-    项目成果：zabbix主要监控服务器宿主机、虚拟机性能，Prometheus监控各种项目返回时间/占用率等指标、各种云平台余额/套餐剩余等`,
-        },
-        {
-            title: '运维系统编写',
-            content: `项目思路：上百个服务器、服务、域名信息没地方登记，测试、运营等同事经常私聊问；服务上线时自动上报版本信息，新增服务器就登记
-    项目成果：用fastapi+vue完成编写，全公司权限分离自行查询上千个服务器、数据库，降低运维重复的工作`,
-        },
-    ];
-
     return (
-        <div className="w-full h-full p-6 flex">
-            <div className="w-[20%] flex items-center justify-center">
+        <div className="w-full h-full p-6 flex flex-col md:flex-row">
+            <div className="w-full md:w-[20%] flex items-center justify-center mb-4 md:mb-0">
                 <Button
                     theme="outline"
                     type="tertiary"
@@ -96,8 +73,8 @@ const WorkProjectMuen = () => {
                     跳转官网
                 </Button>
             </div>
-            <div className="border-r border-white mx-3 h-full" />
-            <div className="w-[80%] flex flex-col gap-4">
+            <div className="border-r-0 md:border-r border-white mx-0 md:mx-3 h-px md:h-full w-full md:w-px my-4 md:my-0" />
+            <div className="w-full md:w-[80%] flex flex-col gap-4">
                 <AnimatePresence>
                     {selectedProject ? (
                         <motion.div
@@ -106,7 +83,7 @@ const WorkProjectMuen = () => {
                             initial={{ opacity: 0 }}
                             animate={{ opacity: 1 }}
                             exit={{ opacity: 0 }}
-                            transition={{ duration: 0.5 }}>
+                            transition={{ opacity: { duration: 0.3 } }}>
                             <button
                                 className="absolute top-5 left-10 text-sm"
                                 onClick={() => setSelectedProject(null)}>
@@ -128,9 +105,9 @@ const WorkProjectMuen = () => {
                             initial={{ opacity: 0 }}
                             animate={{ opacity: 1 }}
                             exit={{ opacity: 0 }}
-                            transition={{ duration: 0.5 }}>
-                            <div className="grid grid-cols-2 gap-4 h-full">
-                                {projects.map((project, index) => (
+                            transition={{ opacity: { duration: 0.3 } }}>
+                            <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 h-full">
+                                {muenProjects.map((project, index) => (
                                     <motion.div
                                         key={index}
                                         className="p-4 bg-white/10 hover:bg-white/20 transition-colors cursor-pointer flex items-center justify-center"
@@ -138,7 +115,7 @@ const WorkProjectMuen = () => {
                                         onClick={() =>
                                             setSelectedProject(project)
                                         }>
-                                        <div className="text-center">
+                                        <div className="text-center text-sm sm:text-base">
                                             {project.title}
                                         </div>
                                     </motion.div>
