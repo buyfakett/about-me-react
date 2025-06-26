187664def5a21abf75acd7bdf8681cc0875631d2
buyfakett <buyfakett@vip.qq.com>
2025-06-26 13:56:17 +0800

    升级依赖 增加按需编译

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+// tailwind.config.js
+export default {
+    content: [
+        "./src/**/*.{html,js,ts,jsx,tsx}"
+    ],
+    safelist: [
+        'bg-blue-500',
+        'bg-yellow-500',
+        'bg-gray-400',
+        'bg-gray-500',
+        'bg-red-500',
+        'bg-green-500',
+        'bg-sky-500',
+        'bg-indigo-500',
+        'bg-lime-500',
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
